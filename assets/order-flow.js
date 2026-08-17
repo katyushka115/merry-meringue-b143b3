1ecd1cf52bd7fb1b05302fec80ea480053300c83
katyushka115 <ekovalenko2002@gmail.com>
2026-08-17 11:30:09 +0300

    Load safe studio contact order patch

--- assets/order-flow.js
+++ assets/order-flow.js
@@ -8,5 +8,6 @@
   });
   load('assets/order-flow-core.js?v=20260816-safe')
     .then(() => load('assets/site-content-loader.js?v=20260816-1'))
+    .then(() => load('assets/studio-contact-order.js?v=20260817-1'))
     .catch(err => console.error('SM Flowers loader error:', err));
 })();
