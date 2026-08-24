b8fc861da2e942cbd5edfd69c046bdb40a5a7f6c
katyushka115 <ekovalenko2002@gmail.com>
2026-08-24 19:29:02 +0300

    Route browser Supabase client through same-origin proxy

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,4 +1,4 @@
 window.SM_SUPABASE_CONFIG = Object.freeze({
-  url: 'https://avlozhwwvjqiypifoxox.supabase.co',
+  url: '/supabase',
   publishableKey: 'sb_publishable_3FgdTAmKB8kw2QTrrVPA5g_vb1lya1d'
 });
